dbcef89a7779003a9a5b66c0485a9283996d54ab
Noelle Davis <noelle798@gmail.com>
2019-12-08 17:10:45 -0800

    added lcd i2c driver

--- software/lcd.c
+++ software/lcd.c
@@ -1,5 +1,126 @@
 #include "lcd.h"
 
+// https://github.com/tomozh/arduino_ST7032
+
+
+void i2c_out(unsigned char j)
+{
+	int n;
+	unsigned char d;
+	d = j;
+
+	for(n=0; n<8; n++) {
+		if((d & 0x80) == 0x80)
+			SDA = 1;
+		else
+			SDA = 0;
+
+		d = (d<<1);
+		SCL = 0;
+		SCL = 1;
+		SCL = 0;
+	}
+
+	SCL = 1;
+	while(SDA == 1) {	// gets caught here if no slave acknowledge
+		SCL = 0;
+		SCL = 1;
+	}
+	SCL = 0;
+}
+
+void i2c_start(void)
+{
+	SCL = 1;
+	SDA = 1;
+	SDA = 0;
+	SCL = 0;
+}
+
+void i2c_stop(void)
+{
+	SDA = 0;
+	SCL = 0;
+	SCL = 1;
+	SDA = 1;
+}
+
+void i2c_begin_tx(unsigned char slave_addr) {
+	i2c_start();
+	i2c_out((slave_addr << 1) | I2C_WR);
+}
+
+void send_cmd(unsigned char cmd) {
+	i2c_begin_tx(LCD_I2C);
+	i2c_out(CMD_CTL);
+	i2c_out(cmd);
+	i2c_stop();
+
+	// add 30us delay?
+}
+
+void send_data(unsigned char *text) {
+	i2c_begin_tx(LCD_I2C)
+	i2c_out(DATA_CTL);
+	for(int n=0; n<16; n++) {
+		i2c_out(*text);
+		//hold for busy flag
+		text++;
+	}
+	i2c_stop();
+
+	// add 30us delay?
+}
+
+void wait_busy() {
+	/*
+	i2c:
+		i2c addr WR
+		read busy flag cmd
+	i2c:
+		i2c addr RD
+		loop:
+			read data
+			check busy flag
+	*/
+}
+
+void lcd_show(unsigned char *text)	// 16 chars
+{
+	send_cmd(CLR_DISP_CMD);	// clear display command - also sets DDRAM address to 0
+	// hold for busy flag
+	// set ddram address? (or just set to home - this unnecessary since not shifting)
+	// hold for busy flag
+	send_data(text);	// write data to DDRAM
+}
+
+void init_lcd()
+{
+	// wait 40ms
+	i2c_begin_tx();
+
+	i2c_out(FUNC_SET_CMD | I2C_MODE | TWO_LN_MODE);
+	// 26.3us delay
+	i2c_out(FUNC_SET_CMD | I2C_MODE | TWO_LN_MODE);
+	// 26.3us delay
+	i2c_out(INT_OSC_CMD);
+	// 26.3us delay
+	i2c_out(CTRST_CMD);
+	// 26.3us delay
+	i2c_out(PWR_ICON_CMD | BOOSTER_ON);
+	// 26.3us delay
+	i2c_out(FOLLOW_CMD | FOLLOW_ON);
+	// 200ms delay
+	i2c_out(DISP_CTL_CMD | DISP_ON | CURS_ON | CURS_BLINK_ON);
+	// 26.3us delay
+	i2c_out(CLR_DISP_CMD);
+	// 26.3us delay
+	i2c_out(ENTRY_MODE_CMD | CURS_LR);
+	// 26.3us delay
+
+	i2c_stop();
+}
+
 void lcd_display(int r, int c, char *str) {
     // TODO: complete
 }
--- software/lcd.h
+++ software/lcd.h
@@ -1,6 +1,55 @@
 #ifndef LCD_H
 #define LCD_H
 
-void lcd_display(int r, int c, char *str);
+#define LCD_I2C		0b0111110	// slave address
+#define I2C_RD		1
+#define	I2C_WR		0
 
+#define	CMD_CTL		0b00000000
+					//0------- only one ctl byte per transmission
+					//-0------ reg sel = 0, sending commmand
+					//--000000 ctl byte
+
+#define DATA_CTL	0b01000000
+					//0------- only one ctl byte per transmission
+					//-1------ reg sel = 1, sending data
+					//--000000 ctl byte
+
+#define CLR_DISP_CMD	0b00000000
+
+#define	RTN_HOME_CMD	0b00000010
+
+#define	ENTRY_MODE_CMD	0b00000100
+#define	CURS_LR			0b00000010	// cursor moves L->R
+
+#define DISP_CTL_CMD	0b00001000
+#define DISP_ON			0b00000100
+#define	CURS_ON			0b00000010
+#define	CURS_BLINK_ON	0b00000001
+
+#define FUNC_SET_CMD	0b00100000
+#define	I2C_MODE		0b00010000
+#define	TWO_LN_MODE		0b00001000
+#define	DBL_HI_MODE		0b00000100	// double height disp, incompatible with two-line mode
+#define	EXT_INST_MODE	0b00000001	// extended instruction set
+
+#define INT_OSC_CMD		0b00010100
+
+#define CTRST_CMD		0b01110000	// *may need to set last four bits (C3..0) for ref voltage V0
+#define CTRST_HI		0b00001000 	// *from example
+
+#define	PWR_ICON_CMD	0b01010000	// *may need to set last 2 bits (C5..4)
+#define	ICON_MODE		0b00001000	// icon display on
+#define BOOSTER_MODE	0b00000100	// booster mode on
+#define CTRST_LO		0b00000010	// *from example
+
+#define	FOLLOW_CMD		0b01100000	// *may need to set last 3 bits (Rab2..0) for V0 generator
+#define	FOLLOW_ON		0b00001000	// follower circuit on
+#define V0_AMP_RATIO	0b00000010	// *from example
+
+#define	DDR_ADDR_CMD	0b10000000	// set DDRAM address
+
+void init_lcd();
+void lcd_show(unsigned char *text);
+void lcd_display(int r, int c, char *str);
 #endif // LCD_H
