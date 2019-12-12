a2050722a1b6f423589557b795aeeba3a846b7fb
Noelle Davis <noelle798@gmail.com>
2019-12-11 22:09:06 -0800

    updated lcd driver for twi

--- software/lcd.c
+++ software/lcd.c
@@ -1,8 +1,120 @@
 #include "lcd.h"
+#include <util/delay.h>
 
 // https://github.com/tomozh/arduino_ST7032
 
 
+int send_cmd(unsigned char cmd) {
+
+	tx_type *tx_frame;
+	unsigned char temp[2];
+	char state = 'n';
+		
+	tx_frame->slave_adr = LCD_I2C+W;		//Slave adr + Write
+	tx_frame->size = 2;					//Number of bytes to send
+	tx_frame->data_ptr = temp;			//Set the pointer to temp array	0
+	tx_frame->data_ptr[0] = CMD_CTL;		
+	tx_frame->data_ptr[1] = cmd;	
+
+	state = Send_to_TWI(tx_frame);			//Call the Master TWI driver with
+											//a pointer to the first struct 
+											//in the package
+											
+	return state;							//If error occured during the TWI
+											//comunication, return TWSR
+											//If  no error, return SUCCESS		
+
+	// add 30us delay?
+}
+
+int send_data(unsigned char *text) {
+
+	tx_type *tx_frame;
+	unsigned char temp[17];
+	char state = 'n';
+		
+	tx_frame->slave_adr = LCD_I2C+W;		//Slave adr + Write
+	tx_frame->size = 17;					//Number of bytes to send
+	tx_frame->data_ptr = temp;			//Set the pointer to temp array	0
+	tx_frame->data_ptr[0] = DATA_CTL;
+
+	for(int i=0; i<16; i++)
+		tx_frame->data_ptr[i+1] = text[i];	
+
+	state = Send_to_TWI(tx_frame);			//Call the Master TWI driver with
+											//a pointer to the first struct 
+											//in the package
+											
+	return state;							//If error occured during the TWI
+											//comunication, return TWSR
+											//If  no error, return SUCCESS
+
+	// add 30us delay?
+}
+
+void wait_busy() {
+	/*
+	i2c:
+		i2c addr WR
+		read busy flag cmd
+	i2c:
+		i2c addr RD
+		loop:
+			read data
+			check busy flag
+	*/
+}
+
+void lcd_show(unsigned char *text)	// 16 chars
+{
+	send_cmd(CLR_DISP_CMD);	// clear display command - also sets DDRAM address to 0
+	// hold for busy flag
+	// set ddram address? (or just set to home - this unnecessary since not shifting)
+	// hold for busy flag
+	send_data(text);	// write data to DDRAM
+}
+
+int init_lcd()
+{
+	Init_TWI();			// set baud rate and enable TWI
+
+	// wait 40ms
+	_delay_ms(40);
+
+	char state = 'n';
+	tx_type *tx_frame;
+	
+	unsigned char temp[9] = {
+		FUNC_SET_CMD | I2C_MODE | TWO_LN_MODE,
+		FUNC_SET_CMD | I2C_MODE | TWO_LN_MODE,
+		INT_OSC_CMD,
+		CTRST_CMD,
+		PWR_ICON_CMD | BOOSTER_MODE,
+		FOLLOW_CMD | FOLLOW_ON,
+		DISP_CTL_CMD | DISP_ON | CURS_ON | CURS_BLINK_ON,
+		CLR_DISP_CMD,
+		ENTRY_MODE_CMD | CURS_LR
+	};
+		
+	tx_frame->slave_adr = LCD_I2C+W;		//Slave adr + Write
+	tx_frame->size = 9;					//Number of bytes to send
+	tx_frame->data_ptr = temp;			//Set the pointer to temp array	0
+
+	state = Send_to_TWI(tx_frame);			//Call the Master TWI driver with
+											//a pointer to the first struct 
+											//in the package
+											
+	return state;							//If error occured during the TWI
+											//comunication, return TWSR
+											//If  no error, return SUCCESS
+}
+
+void lcd_display(int r, int c, char *str) {
+    // TODO: complete
+}
+
+
+/*
 void i2c_out(unsigned char j)
 {
 	int n;
@@ -49,78 +161,4 @@ void i2c_begin_tx(unsigned char slave_addr) {
 	i2c_start();
 	i2c_out((slave_addr << 1) | I2C_WR);
 }
-
-void send_cmd(unsigned char cmd) {
-	i2c_begin_tx(LCD_I2C);
-	i2c_out(CMD_CTL);
-	i2c_out(cmd);
-	i2c_stop();
-
-	// add 30us delay?
-}
-
-void send_data(unsigned char *text) {
-	i2c_begin_tx(LCD_I2C)
-	i2c_out(DATA_CTL);
-	for(int n=0; n<16; n++) {
-		i2c_out(*text);
-		//hold for busy flag
-		text++;
-	}
-	i2c_stop();
-
-	// add 30us delay?
-}
-
-void wait_busy() {
-	/*
-	i2c:
-		i2c addr WR
-		read busy flag cmd
-	i2c:
-		i2c addr RD
-		loop:
-			read data
-			check busy flag
-	*/
-}
-
-void lcd_show(unsigned char *text)	// 16 chars
-{
-	send_cmd(CLR_DISP_CMD);	// clear display command - also sets DDRAM address to 0
-	// hold for busy flag
-	// set ddram address? (or just set to home - this unnecessary since not shifting)
-	// hold for busy flag
-	send_data(text);	// write data to DDRAM
-}
-
-void init_lcd()
-{
-	// wait 40ms
-	i2c_begin_tx();
-
-	i2c_out(FUNC_SET_CMD | I2C_MODE | TWO_LN_MODE);
-	// 26.3us delay
-	i2c_out(FUNC_SET_CMD | I2C_MODE | TWO_LN_MODE);
-	// 26.3us delay
-	i2c_out(INT_OSC_CMD);
-	// 26.3us delay
-	i2c_out(CTRST_CMD);
-	// 26.3us delay
-	i2c_out(PWR_ICON_CMD | BOOSTER_ON);
-	// 26.3us delay
-	i2c_out(FOLLOW_CMD | FOLLOW_ON);
-	// 200ms delay
-	i2c_out(DISP_CTL_CMD | DISP_ON | CURS_ON | CURS_BLINK_ON);
-	// 26.3us delay
-	i2c_out(CLR_DISP_CMD);
-	// 26.3us delay
-	i2c_out(ENTRY_MODE_CMD | CURS_LR);
-	// 26.3us delay
-
-	i2c_stop();
-}
-
-void lcd_display(int r, int c, char *str) {
-    // TODO: complete
-}
+*/
--- software/lcd.h
+++ software/lcd.h
@@ -1,6 +1,9 @@
 #ifndef LCD_H
 #define LCD_H
 
+#include "TWI_driver.h"
+
+
 #define LCD_I2C		0b0111110	// slave address
 #define I2C_RD		1
 #define	I2C_WR		0
@@ -49,7 +52,7 @@
 
 #define	DDR_ADDR_CMD	0b10000000	// set DDRAM address
 
-void init_lcd();
+int init_lcd();
 void lcd_show(unsigned char *text);
 void lcd_display(int r, int c, char *str);
 #endif // LCD_H
